645e57c703832c85aa8830d9bca52fbca0c1e45f
AxelWyart <axelwyart@gmail.com>
2026-08-22 10:49:49 +0200

    Replace Tailwind CDN with local build

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  darkMode: "media",
+  theme: {
+    extend: {
+      screens: {
+        "3xl": "1920px"
+      }
+    }
+  },
+  plugins: []
+};
